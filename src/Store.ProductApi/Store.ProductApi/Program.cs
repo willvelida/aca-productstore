42ef3ce4ab509f80c9d05f203f4fd0d598d21f14
willvelida <willvelida@hotmail.co.uk>
2023-05-09 03:51:25 +0100

    Adding ProductApi

---
+++ src/Store.ProductApi/Store.ProductApi/Program.cs
@@ -0,0 +1,37 @@
+using Bogus;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var products = new Faker<Product>()
+    .StrictMode(true)
+    .RuleFor(p => p.ProductId, (f, p) => f.Database.Random.Guid())
+    .RuleFor(p => p.ProductName, (f, p) => f.Commerce.ProductName())
+    .RuleFor(p => p.Manufacturer, (f, p) => "")
+    .Generate(10);
+
+app.MapGet("/products", () => Results.Ok(products))
+    .Produces<Product[]>(StatusCodes.Status200OK)
+    .WithName("GetProducts");
+
+app.Run();
+
+public class Product
+{
+    public Guid ProductId => Guid.NewGuid();
+    public string ProductName { get; set; }
+    public string Manufacturer { get; set; }
+}
